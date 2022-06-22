30d5f403fda0fd86bae7a1ed7757d7eb967c58e6
nicolasbarlatier <nicolas.barlatier@misterfly.com>
2022-06-22 22:21:50 +0200

    Refactoring

---
+++ JWTWithRS256/JWSWithRS256.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Security.Cryptography;
+
+namespace JWTWithRS256
+{
+    public static class JWSWithRS256
+    {
+        /// <summary>
+        /// To validate the content of the JWT with the signature.
+        /// The signature is creating by the RSA encoding of the SHA-256 hash of the content of headerBase64UrlEncoded.payloadBase64UrlEncoded 
+        /// </summary>
+        /// <param name="JWSWithRS256">JWS content of headerBase64UrlEncoded.payloadBase64UrlEncoded.signatureBaseURlEncoded</param>
+        /// <param name="publicKeyRSABase64UrlEncoded">publickey of RSA base64 url encoded which will be used to validate the SHA-256 hash from the signature</param>
+        /// <returns>True if JWS has header and payload valid with the signature</returns>
+        public static bool IsJWTValidFromRS256(string JWSWithRS256, string publicKeyRSABase64UrlEncoded)
+        {
+            try
+            {
+                #region === Step 1: Separate the 3 JWS part ===
+
+                var JWSParts = JWSWithRS256.Split('.');
+
+                var headerBase64UrlEncoded = JWSParts[0];
+                var payloadBase64UrlEncoded = JWSParts[1];
+                var signatureBase64UrlEncoded = JWSParts[2];
+
+                #endregion
+
+                #region === Step 2: Get SHA256HashToBeSigned from {headerBase64UrlEncoded.payloadBase64UrlEncoded} ===
+
+                var SHA256HashToBeSigned = BuildJWTHeaderPayloadSHA256Hash(headerBase64UrlEncoded, payloadBase64UrlEncoded);
+
+                #endregion
+
+                #region === Step 3: Decode the signature from Base64Url into the signature byte[] array ===
+
+                var signatureInBytes = Base64UrlEncoder.DecodeBytes(signatureBase64UrlEncoded);
+
+                #endregion
+
+                #region === Step 4: Build RSA from the public key extracted from the SubjectPublickKeyInfo ===
+
+                using RSACryptoServiceProvider rsaCryptoServiceProvider = new RSACryptoServiceProvider(2048);
+
+                var publicKey = Base64UrlEncoder.DecodeBytes(publicKeyRSABase64UrlEncoded);
+
+                rsaCryptoServiceProvider.ImportSubjectPublicKeyInfo(
+                    source: publicKey,
+                    bytesRead: out _
+                );
+
+                #endregion
+
+                #region === Step 5: Verification that the SHA-256 hash made from the JWS header and payload to be signed matches the one from the Signature ===
+
+                var SHA256ObjectIdentifier = CryptoConfig.MapNameToOID("SHA256");
+
+                return rsaCryptoServiceProvider.VerifyHash(SHA256HashToBeSigned, SHA256ObjectIdentifier, signatureInBytes);
+
+                #endregion
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Build the SHA-256 Hash from the JWs Header and payload which was used to be signed in the JWS.
+        /// This Hash will be used to check if it maches the Hash from the signature made from SHA-256 and RSA encoding with the privatekey
+        /// The method will do: SHA-256(headerBase64UrlEncoded + . + payloadBase64UrlEncoded)
+        /// </summary>
+        /// <param name="headerBase64UrlEncoded">JWS Header base64 url encoded</param>
+        /// <param name="payloadBase64UrlEncoded">JWS Payload base64 url encoded</param>
+        /// <returns></returns>
+        private static byte[] BuildJWTHeaderPayloadSHA256Hash(string headerBase64UrlEncoded, string payloadBase64UrlEncoded)
+        {
+            return SHA256.Create().ComputeHash(
+               System.Text.Encoding.UTF8.GetBytes($"{headerBase64UrlEncoded}.{payloadBase64UrlEncoded}"));
+        }
+    }
+}
--- JWTWithRS256/Program.cs
+++ JWTWithRS256/Program.cs
@@ -13,83 +13,10 @@ namespace JWTWithRS256
 
             var publicKeyBase64UrlEncoded = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAi6G5wRCfqjXJFBhvK+UwAUFU9LDcT3aet0gGZk8hMMPfF5SEBaPqTDuLMh85VXtm0I0KBpUtlgNzMqcmWVoSFTNSSJnmBBmD/26xcidu/wuo4m3wTIca2kOLBtMP/3sjIDOXAQYaCOXjbbDbNB1S49VD6wUyJy8gGwiTsDzuZcNsS5c+hnAiI+WHqUnSll/EGZcKp1Yv7BZH9fYRXdTGYRGcH6ZRH8Nhl9w6QL+gSRA2wZLjS9r5NdZ5Ey8iSezs1Htdtg2sj0mA1QlvdOkPQVzD5hW80it5sHMY0l1W1XJSPnkdGNaTJXyn5Fto15uJhj5nE6MwkwiTfGlwxmZQLwIDAQAB";
 
-            Console.WriteLine($"JWT valid ? {IsJWTValidFromRS256(JWSWithRS256, publicKeyBase64UrlEncoded)}");
+            Console.WriteLine($"JWT valid ? {JWTWithRS256.JWSWithRS256.IsJWTValidFromRS256(JWSWithRS256, publicKeyBase64UrlEncoded)}");
 
         }
 
-        /// <summary>
-        /// To validate the content of the JWT with the signature.
-        /// The signature is creating by the RSA encoding of the SHA-256 hash of the content of headerBase64UrlEncoded.payloadBase64UrlEncoded 
-        /// </summary>
-        /// <param name="JWSWithRS256">JWS content of headerBase64UrlEncoded.payloadBase64UrlEncoded.signatureBaseURlEncoded</param>
-        /// <param name="publicKeyRSABase64UrlEncoded">publickey of RSA base64 url encoded which will be used to validate the SHA-256 hash from the signature</param>
-        /// <returns>True if JWS has header and payload valid with the signature</returns>
-        private static bool IsJWTValidFromRS256(string JWSWithRS256, string publicKeyRSABase64UrlEncoded)
-        {
-            try
-            {
-                #region === Step 1: Separate the 3 JWS part ===
-
-                var JWSParts = JWSWithRS256.Split('.');
-
-                var headerBase64UrlEncoded = JWSParts[0];
-                var payloadBase64UrlEncoded = JWSParts[1];
-                var signatureBase64UrlEncoded = JWSParts[2];
-
-                #endregion
-
-                #region === Step 2: Get SHA256HashToBeSigned from {headerBase64UrlEncoded.payloadBase64UrlEncoded} ===
-
-                var SHA256HashToBeSigned = BuildJWTHeaderPayloadSHA256Hash(headerBase64UrlEncoded, payloadBase64UrlEncoded);
-
-                #endregion
-
-                #region === Step 3: Decode the signature from Base64Url into the signature byte[] array ===
-
-                var signatureInBytes = Base64UrlEncoder.DecodeBytes(signatureBase64UrlEncoded);
-
-                #endregion
-
-                #region === Step 4: Build RSA from the public key extracted from the SubjectPublickKeyInfo ===
-
-                using RSACryptoServiceProvider rsaCryptoServiceProvider = new RSACryptoServiceProvider(2048);
-
-                var publicKey = Base64UrlEncoder.DecodeBytes(publicKeyRSABase64UrlEncoded);
-
-                rsaCryptoServiceProvider.ImportSubjectPublicKeyInfo(
-                    source: publicKey,
-                    bytesRead: out _
-                );
-
-                #endregion
-
-                #region === Step 5: Verification that the SHA-256 hash made from the JWS header and payload to be signed matches the one from the Signature ===
-
-                var SHA256ObjectIdentifier = CryptoConfig.MapNameToOID("SHA256");
-                
-                return rsaCryptoServiceProvider.VerifyHash(SHA256HashToBeSigned, SHA256ObjectIdentifier, signatureInBytes);
-
-                #endregion
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                return false;
-            }           
-        }
-
-        /// <summary>
-        /// Build the SHA-256 Hash from the JWs Header and payload which was used to be signed in the JWS.
-        /// This Hash will be used to check if it maches the Hash from the signature made from SHA-256 and RSA encoding with the privatekey
-        /// The method will do: SHA-256(headerBase64UrlEncoded + . + payloadBase64UrlEncoded)
-        /// </summary>
-        /// <param name="headerBase64UrlEncoded">JWS Header base64 url encoded</param>
-        /// <param name="payloadBase64UrlEncoded">JWS Payload base64 url encoded</param>
-        /// <returns></returns>
-        private static byte[] BuildJWTHeaderPayloadSHA256Hash(string headerBase64UrlEncoded, string payloadBase64UrlEncoded)
-        {
-            return SHA256.Create().ComputeHash(
-               System.Text.Encoding.UTF8.GetBytes($"{headerBase64UrlEncoded}.{payloadBase64UrlEncoded}"));
-        }
+       
     }
 }
